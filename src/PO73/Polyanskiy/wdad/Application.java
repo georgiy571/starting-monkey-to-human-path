ab33bc92bf7937e158db82da5c428b988d6e59db
georgiy571 <polyanskiy-g@mail.ru>
2019-09-19 16:03:43 +0400

    Added entry point

---
+++ src/PO73/Polyanskiy/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO73.Polyanskiy.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Georgiy Polyanskiy, and I'm not a monkey");
+    }
+}
