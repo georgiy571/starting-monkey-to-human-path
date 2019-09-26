27269abe9c7f1dd7d0b5639156acd19b9e4386be
georgiy571 <polyanskiy-g@mail.ru>
2019-09-26 16:49:25 +0400

    Document created in XmlTask

--- src/PO73/Polyanskiy/wdad/learn/xml/XmlTask.java
+++ src/PO73/Polyanskiy/wdad/learn/xml/XmlTask.java
@@ -1,7 +1,21 @@
 package PO73.Polyanskiy.wdad.learn.xml;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class XmlTask {
-    public String getNoteText (User owner, String title){
+    public static final String FILE_PATH = "";
+
+    public String getNoteText (User owner, String title) {
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(FILE_PATH);
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
